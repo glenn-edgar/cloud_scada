35e38d5e12f320e597043976ec8505c5a9d474af
Glenn Edgar <glenn-edgar@onyxengr.com>
2016-01-10 19:30:24 +0000

    initial

---
+++ mongodb/collection_functions.py
@@ -0,0 +1,181 @@
+import json
+import pymongo
+from pymongo import MongoClient
+
+
+class Mongodb_DataBase():
+
+   def __init__( self, client ):
+      self.client = client
+      
+   def list_databases(self):
+      return self.client.database_names()
+   
+
+   def create_database( self,database_name):
+      return self.client[database_name]
+
+   def remove_all_databases( self ):
+       databases = self.list_databases()
+       for i in databases:
+          self.remove_database(i)
+    
+
+   def remove_database( self,database_name ):
+       self.client.drop_database( database_name)
+
+
+class Mongodb_Collection():
+
+   def __init__(self,client ):
+     self.client          = client
+     
+
+   def collection_names( self,db_name ):
+       db = self.client[ db_name ]
+       return db.collection_names()
+
+
+   def collection_exits( self,db_name, collection_name  ):
+     db = self.client[db_name]
+     if collection_name in list(db.collection_names()):
+          return True
+     else:
+          return False
+       
+
+   def create_collection( self,db_name, collection_name, capped=False, max_number=0,collection_size=0 ):
+     db = self.client[db_name]
+     if collection_name in list(db.collection_names()):
+         pass # print "duplicate collection db "+db_name + " collection name "+ collection_name
+     else:
+          db.create_collection( collection_name, capped=capped, max=max_number,size=collection_size)
+     
+
+
+   def collection_number( self, db_name, collection_name):
+       db = self.client[ db_name ]
+       if collection_name in list(db.collection_names()):
+          collection = db[ collection_name ]
+          return collection.count()
+       else:
+          raise
+
+    
+
+   
+   def delete_collection( self, db_name, collection_name):
+       db = self.client[ db_name ]
+       if collection_name in list(db.collection_names()):
+          collection = db[ collection_name ]
+          collection.drop()
+       else:
+          raise
+
+   def insert_document( self, db_name, collection_name, document_list):
+      db = self.client[ db_name ]
+     
+      if collection_name in list(db.collection_names()):
+          collection = db[ collection_name ]
+          for i in document_list:
+             collection.insert(i)
+      else:
+          raise
+
+   def find_document( self, db_name="", collection_name="", query="", query_number=0, index=0, sort_field = "", ascending_sort = True  ):
+      db = self.client[ db_name ]
+      return_value = None
+      if collection_name in list(db.collection_names()):
+          collection = db[ collection_name ]
+          if query_number == 0:
+              query_number = collection.count()
+          if ascending_sort == True:
+              return_value = list(collection.find(query, skip=index , limit=query_number, sort = [[ sort_field, pymongo.ASCENDING ]] )) 
+          else:
+              return_value = list(collection.find(query, skip=index, limit=query_number, sort=[[ sort_field, pymongo.DESCENDING ]] ))  
+      else:
+          raise
+      return return_value
+
+
+   def remove_document( self, db_name, collection_name, query  ):
+      db = self.client[ db_name ]
+      
+      if collection_name in list(db.collection_names()):
+          collection = db[ collection_name ]
+          collection.remove( query )
+      else:
+          raise
+      
+ 
+   def remove_all_documents( self,db_name,collection_name ):
+      db = self.client[ db_name ]
+      return_value = 0
+      if collection_name in list(db.collection_names()):
+          collection = db[ collection_name ]
+          collection.remove( {} )
+      else:
+          raise
+      
+   def modify_documents( self, db_name, collection_name, query, set_query, multi_flag = False):
+      db = self.client[ db_name ]
+      return_value = 0
+      if collection_name in list(db.collection_names()):
+          collection = db[ collection_name ]
+          collection.update( query, { "$set":set_query } , multi = multi_flag )
+      else:
+          raise
+
+
+   def collection_tail( self, db_name="", collection_name="", query="", query_number=0, index=0,sort_field = ""  ):
+      db = self.client[ db_name ]
+      return_value = None
+      if collection_name in list(db.collection_names()):
+          collection = db[ collection_name ]
+          if query_number == 0:
+              query_number = collection.count()
+          new_index = collection.count() - index -1
+          if new_index < 0 :
+               new_index = 0
+          print new_index,query_number
+          return_value = list(collection.find(query, skip=new_index, limit=query_number, sort=[[ sort_field, pymongo.ASCENDING ]] ))
+          return_value.reverse()
+      else:
+          raise
+      return return_value
+
+
+
+
+if __name__ == "__main__":
+   client          = MongoClient()
+   mongodb_db      = Mongodb_DataBase( client )
+   mongodb_col     = Mongodb_Collection( client)
+#   This section tests list and remove data bases
+#   print mongodb.list_databases()   
+#   mongodb.remove_all_databases()
+#   print mongodb.list_databases() 
+
+# create a test data base
+   mongodb_db.remove_all_databases()
+   print mongodb_db.list_databases()
+   mongodb_db.create_database( "test")
+ 
+   mongodb_col.create_collection("test","collection",capped = False, max_number= 255, collection_size = 100000)
+   print mongodb_col.collection_number(  "test", "collection" )
+   print mongodb_db.list_databases()
+   mongodb_col.delete_collection( "test", "collection")
+   mongodb_col.create_collection("test","collection",capped= False,max_number= 255, collection_size =100000)
+   mongodb_col.insert_document( "test", "collection", [{"name":34,"date":11 }])
+   print mongodb_col.collection_number(  "test", "collection" )
+   mongodb_col.insert_document( "test", "collection", [{"name":34,"date":22 }])
+   mongodb_col.insert_document( "test", "collection", [{"name":34,"date":33 }])
+   print mongodb_col.collection_number(  "test", "collection" )
+   print mongodb_col.find_document( db_name="test", collection_name="collection",query={"name":34}, index=1, query_number=1, sort_field = "date", ascending_sort = True  )
+   print mongodb_col.find_document( db_name="test", collection_name="collection",query={"name":34}, index=0, query_number=2, sort_field = "date", ascending_sort = True  )
+   print mongodb_col.find_document( db_name="test", collection_name="collection",query={"name":34}, index=0, query_number=0, sort_field = "date", ascending_sort = True  )
+   print mongodb_col.find_document( db_name="test", collection_name="collection",query={"name":34}, index=0, query_number=0, sort_field = "date", ascending_sort = False  )
+   print mongodb_col.collection_tail( db_name="test", collection_name = "collection", query={"name":34}, index=1, query_number=0, sort_field = "date")
+   #print mongodb_col.find_document( "test", "collection",{"name":34}, 0, 0, ascending_sort = False  )
+
+
